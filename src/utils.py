eb3384a8d79d7f7552322fdac9f50853387b6475
Tony Benoy <me@tonybenoy.com>
2020-12-29 01:12:59 +0530

    missing code

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from os import path
 import httpx
 from typing import List, Dict
+from re import compile
+from urllib.parse import parse_qsl, urlsplit
 
 templates = Jinja2Templates(directory="src/templates")
 
@@ -66,6 +68,30 @@ def update_count() -> int:
     db_conn.commit()
     db_conn.close()
     return count
+
+
+rlink = compile(r'<(.*?)>(.*?)rel="([A-z\s]*)"(.*?)(?:$|(?:,))')
+assignations = compile(r'([A-z]*?)="(.*?)"')
+
+
+def parse(link_header: str) -> Dict[str, Dict[str, str]]:
+    # shamelessly copied from https://github.com/FlorianLouvetRN/linkheader_parser/blob/master/linkheader_parser/parser.py
+    links = {}
+    for match in rlink.finditer(link_header):
+        parsed_content = {}
+        link = match.group(1)
+        rel = match.group(3)
+        parsed_content["url"] = link
+        query_params = dict(parse_qsl(urlsplit(link).query))
+        parsed_content = {**parsed_content, **query_params}
+        extra = match.group(2)
+        if match.group(4) is not None:
+            extra += match.group(4)
+        for a in assignations.finditer(extra):
+            parsed_content[a.group(1)] = a.group(2)
+        for r in rel.split(" "):
+            links[r] = {**parsed_content, **{"rel": r}}
+    return links
 
 
 def get_repo_data_for_user(
