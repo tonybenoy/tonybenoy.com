5c92abba00bc2a42150088be1b9200c5f835f7b4
Tony Benoy <me@tonybenoy.com>
2020-12-29 02:22:28 +0530

    cache wip

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import redis
 from src.routes.home import home
 from src.routes.apps import apps
 
 app = FastAPI()
 
+r = redis.StrictRedis(host="localhost", port=6379, db=0)
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 app.include_router(home)
 app.include_router(apps)
--- src/routes/apps.py
+++ src/routes/apps.py
@@ -2,16 +2,20 @@ from fastapi import Request
 import httpx
 from fastapi import APIRouter
 from src.utils import templates, get_repo_data_for_user
+from datetime import timedelta
+from src.main import r
 
 apps = APIRouter()
 
 
 @apps.get("/app")
 async def apps_view(request: Request):
-    url = "https://api.github.com/users/tonybenoy/repos?sort=pushed"
-    resp = []
-    resp = get_repo_data_for_user(url=url, response=[])
-
+    resp = r.get("cached_val")
+    if not resp:
+        url = "https://api.github.com/users/tonybenoy/repos?sort=pushed"
+        resp = []
+        resp = get_repo_data_for_user(url=url, response=[])
+        r.setex(name="cached_val", time=timedelta(minutes=60), value=resp)
     return templates.TemplateResponse(
         "apps.html", {"request": request, "title": "My apps", "repos": resp}
     )
